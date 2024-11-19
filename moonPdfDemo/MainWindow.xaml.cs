9398d2ed562a34acf111b85ba49735442bbb26b8
asuka1h <77903223+asuka1h@users.noreply.github.com>
2024-11-19 09:09:10 -0500

    add logi

--- moonPdfDemo/MainWindow.xaml.cs
+++ moonPdfDemo/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using MoonPdfLib;
 
 namespace moonPdfDemo
 {
@@ -20,5 +22,22 @@ namespace moonPdfDemo
         {
             InitializeComponent();
         }
+        private void Pdf_Click(object sender, RoutedEventArgs e)
+        {
+            var fileName = @"Resource\1Where Do My Thoughts Go.pdf";
+            var filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+            pdfPanel.OpenFile(filePath);
+            pdfPanel.Zoom(1.0);
+            this.pdfPanel.ViewType = ViewType.SinglePage;
+        }
+        private void next_Click(object sender, RoutedEventArgs e)
+        {
+            pdfPanel.GotoNextPage();
+            pdfPanel.Zoom(1.0);
+            this.pdfPanel.ViewType = ViewType.SinglePage;
+        }
+
+    }
     }
 }
